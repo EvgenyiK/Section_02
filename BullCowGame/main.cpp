084ad6b0c75d2cf78d90a82c62815cd4c7afaf98
evgen <evgenprogrammer@gmail.com>
2022-04-24 14:15:13 +0300

    encapsulation

--- BullCowGame/main.cpp
+++ BullCowGame/main.cpp
@@ -1,33 +1,52 @@
 #include <iostream>
 #include <string>
 
+
 using namespace std;
 
 
+void PlayGame();
 
-int main() {
-	// introduce the game
-	constexpr int WORD_LENGHT = 5;
+// introduce the game
+void PrintIntro() 
+{
+	constexpr int WORD_LENGHT = 9;
 	cout << "Welcome to Bulls and Cows, a fun word game." << endl;
 	cout << "Can you guess the " << WORD_LENGHT;
 	cout << " letter isogram I,m thinking of?\n";
 	cout << endl;
+	return;
+}
 
+
+string GetGues() 
+{
 	//get a guess from the player
 	cout << "Enter you guess: ";
 	string Guess = "";
 	getline(cin, Guess);
-	
-	//repeat the guess back to them
-	cout << "Your guess was: " << Guess << endl;
+	return Guess;
+}
 
-	//get a guess from the player
-	cout << "Enter you guess: ";
-	getline(cin, Guess);
 
-	//repeat the guess back to them
-	cout << "Your guess was: " << Guess << endl;
 
-	cout << endl;
+//The entry point for our application
+int main() 
+{
+
+	PrintIntro();
+	PlayGame();
 	return 0;
+}
+
+void PlayGame()
+{
+	//loop for the number of turns asking for guesses
+	constexpr int NUMBER_OF_TURNS = 5;
+	for (int i = 1; i <= NUMBER_OF_TURNS; i++)
+	{
+		string st = GetGues();
+		cout << "Your guess was: " << st << endl;
+		cout << endl;
+	}
 }
