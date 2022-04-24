5a01308a508cae6635c275bef060402cfd97c480
evgen <evgenprogrammer@gmail.com>
2022-04-24 11:41:25 +0300

    add const

---
+++ BullCowGame/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+
+
+int main() {
+	constexpr int WORD_LENGHT = 5;
+	cout << "Welcome to Bulls and Cows, a fun word game." << endl;
+	cout << "Can you guess the " << WORD_LENGHT;
+	cout << " letter isogram I,m thinking of?\n";
+	return 0;
+}
