e30a0d63bcc26228548526dd4a819b68e1dda7e1
evgen <evgenprogrammer@gmail.com>
2022-04-24 12:56:12 +0300

    double code

--- BullCowGame/main.cpp
+++ BullCowGame/main.cpp
@@ -1,12 +1,33 @@
 #include <iostream>
+#include <string>
+
 using namespace std;
 
 
 
 int main() {
+	// introduce the game
 	constexpr int WORD_LENGHT = 5;
 	cout << "Welcome to Bulls and Cows, a fun word game." << endl;
 	cout << "Can you guess the " << WORD_LENGHT;
 	cout << " letter isogram I,m thinking of?\n";
+	cout << endl;
+
+	//get a guess from the player
+	cout << "Enter you guess: ";
+	string Guess = "";
+	getline(cin, Guess);
+	
+	//repeat the guess back to them
+	cout << "Your guess was: " << Guess << endl;
+
+	//get a guess from the player
+	cout << "Enter you guess: ";
+	getline(cin, Guess);
+
+	//repeat the guess back to them
+	cout << "Your guess was: " << Guess << endl;
+
+	cout << endl;
 	return 0;
 }
