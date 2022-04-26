650bc66da8c2c19c45d6c7d9ee81c23017c078ae
evgen <evgenprogrammer@gmail.com>
2022-04-26 16:15:20 +0300

    add ascii bull

--- BullCowGame/main.cpp
+++ BullCowGame/main.cpp
@@ -60,6 +60,32 @@ void PlayGame()
 void PrintIntro()
 {
 	std::cout << "\n\n Welcome to Bulls and Cows, a fun word game." << std::endl;
+
+	
+	std::cout <<
+		R"(
+ .-=-.                .-=-.            .=     ,        =.
+  `) )              ( (`      _  _   /'/    )\,/,/(_   \ \
+ .' /                \ '.      `//-.|  (  ,\\)\//\)\/_  ) |
+(  (   _.-'`::`'-._   )  )     //___\   `\\\/\\/\/\\///'  /
+ \  `'`    .::.    `'`  /   ,-"~`-._ `"--'_   `"""`  _ \`'"~-,_
+  `-._\`~`      `~`/_.-'    \       `-.  '_`.      .'_` \ ,-"~`/
+ _,=` /            \ `=,_    `.__.-'`/   (-\        /-) |-.__,'
+(    ,(^\        /^),    )     ||   |     \O)  /^\ (O/  |
+ `~~` \0)\      /(0/ `~~`      `\\  |         /   `\    /
+       | /`    '\ |              \\  \       /      `\ /
+       |          |               `\\ `-.  /' .---.--.\
+       |          |                 `\\/`~(, '()      ('
+       |          |                  /(O) \\   _,.-.,_)
+       /          \                 //  \\ `\'`      /
+       \()      ()/               / |  ||   `""""~"`
+        `._-==-_.'               /'  |__||
+           `""`                        `o
+           BULL           AND                 COW
+                   A funny word game
+
+)" << std::endl;
+
 	std::cout << "Can you guess the " << BCgame.GetHiddenWordLenght();
 	std::cout << " letter isogram I,m thinking of?\n";
 	std::cout << std::endl;
@@ -90,26 +116,26 @@ FText GetValidGues()
 	do {
 		//get a guess from the player
 		int32 CurrentTry = BCgame.GetCurrentTry();
-		std::cout << "Try: " << CurrentTry << ". Enter you guess: ";
+		std::cout << "Try: " << CurrentTry << " of " << BCgame.GetMaxTries();
+		std::cout << ". Enter your guess: ";
 		std::getline(std::cin, Guess);
 
 		Status = BCgame.CheckGuessValidity(Guess);
 		switch (Status)
 		{
 		case EGuessStatus::Not_Isogram:
-			std::cout << "Please enter a word witout repeating letters. \n";
+			std::cout << "Please enter a word witout repeating letters. \n\n";
 			break;
 		case EGuessStatus::Wrong_Lenght:
-			std::cout << "Please enter a " << BCgame.GetHiddenWordLenght() << " letter word. \n";
+			std::cout << "Please enter a " << BCgame.GetHiddenWordLenght() << " letter word. \n\n";
 			break;
 		case EGuessStatus::Not_Lowercase:
-			std::cout << "Please enter all lowercase letters. \n";
+			std::cout << "Please enter all lowercase letters. \n\n";
 			break;
 		default:
 			// assume the guess is valid
 			break;
 		}
-		std::cout << std::endl;
 	} while (Status != EGuessStatus::OK); // keep looping untill we get no errors
 	return Guess;
 }
